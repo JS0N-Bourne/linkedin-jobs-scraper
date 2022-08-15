f25e4e568068156b74706d6a94b2aa99d6078e45
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2022-08-15 02:30:50 +0000

    query clean up

--- index.js
+++ index.js
@@ -1,38 +1,34 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const ObjectsToCsv = require('objects-to-csv');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const ObjectsToCsv = require("objects-to-csv");
 
 linkedinJobs = [];
 
+let keywords = "internship",
+    geoId = "102257491";
 for (let pageNumber = 0; pageNumber < 1000; pageNumber += 25) {
+    let url = `https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search`;
 
-let url = `https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Full%20Stack&location=San%20Antonio%2C%20Texas%20Metropolitan%20Area&geoId=90000724&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=${pageNumber}`
+    axios(url, { params: { keywords, geoId, start: pageNumber } })
+        .then((response) => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const jobs = $("li");
+            jobs.each((index, element) => {
+                const jobTitle = $(element).find("h3.base-search-card__title").text().trim();
+                const company = $(element).find("h4.base-search-card__subtitle").text().trim();
+                const location = $(element).find("span.job-search-card__location").text().trim();
+                const link = $(element).find("a.base-card__full-link").attr("href");
+                linkedinJobs.push({
+                    Title: jobTitle,
+                    Company: company,
+                    Location: location,
+                    Link: link,
+                });
+            });
+            const csv = new ObjectsToCsv(linkedinJobs);
+            csv.toDisk(`./linkedInJobs(${keywords}).csv`, { append: true });
+        })
 
-axios(url)
-
-.then (response => {
-   const html = response.data;
-   const $ = cheerio.load(html);
-   const jobs = $('li');
-     jobs.each((index, element) => {
-
-      const jobTitle = $(element).find('h3.base-search-card__title').text().trim()
-      const company = $(element).find('h4.base-search-card__subtitle').text().trim()
-      const location = $(element).find('span.job-search-card__location').text().trim()
-      const link = $(element).find('a.base-card__full-link').attr('href')
-      linkedinJobs.push({
-        'Title': jobTitle,
-        'Company': company,
-        'Location': location,
-        'Link': link,
-   })
-
-  });
-  const csv = new ObjectsToCsv(linkedinJobs)
-  csv.toDisk('./linkedInJobs.csv', { append: true })
-
-})
-
-.catch(console.error);
-
+        .catch(console.error);
 }
